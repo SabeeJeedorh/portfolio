c9df35314596063000f5af08effe517ed5d851ec
SabeeJeedorh <sabeeajeedorh@gmail.com>
2025-10-29 16:58:27 +0100

    update

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -14,7 +14,7 @@ import mypic from "../assets/mypic.jpg";
 import todo from "../assets/todo.png";
 import roots from "../assets/roots.png";
 import staticSite from "../assets/static-site.png";
-import image3 from "../assets/image.png";
+import image3 from "../assets/todo.png";
 
 const Portfolio = () => {
   const [darkMode, setDarkMode] = useState(false);
